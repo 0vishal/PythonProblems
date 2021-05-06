3a76ef4e2f07768c1629c8b5f5165f1541045047
0vishal <vishalsalaskar72@gmail.com>
2021-05-06 17:23:22 +0530

    [Vishal] Add : added a program to check file exist or not

---
+++ Datastructures/Basic python/Checkfile.py
@@ -0,0 +1,16 @@
+'''
+@Author: Vishal Salaskar
+@Date: 2021-02-29 
+@Last Modified by: Vishal Salaskar
+@Last Modified time: 2021-02-29 
+@Title : To print the calender of given month 
+'''
+
+def check():
+    try:
+        f = open("Vishal.txt")
+        print(f.read())
+    except Exception as e:
+        print(e)
+
+check()
