7914fb5d3d055e0c86194dc60d093f43f76a3c9e
0vishal <vishalsalaskar72@gmail.com>
2021-05-06 17:47:31 +0530

    [Vishal] Add : created a function to list directory

---
+++ Datastructures/Basic python/Listdirectory.py
@@ -0,0 +1,18 @@
+'''
+@Author: Vishal Salaskar
+@Date: 2021-05-06
+@Last Modified by: Vishal Salaskar
+@Last Modified time: 2021-05-06
+@Title : To check files in directory
+'''
+import os 
+
+def list_directory():
+    """
+    Description:
+    Function description :To check files in directory
+    """
+    os.system('ls -l /f/Python/Datastructures')
+
+
+list_directory()
