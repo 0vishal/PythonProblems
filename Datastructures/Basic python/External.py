e30cc7dd0c80a6fadfc86cee05a1128097612851
0vishal <vishalsalaskar72@gmail.com>
2021-05-06 17:33:51 +0530

    [Vishal] Refactor : created a function  to call external command

---
+++ Datastructures/Basic python/External.py
@@ -0,0 +1,17 @@
+'''
+@Author: Vishal Salaskar
+@Date: 2021-05-06
+@Last Modified by: Vishal Salaskar
+@Last Modified time: 2021-05-06
+@Title : To call external command 
+'''
+import os 
+
+def call():
+     """
+    Description:
+    Function description : To call the external command
+    """
+    os.system('ls -l')
+
+call()
