6ffdcaa514e97241d969a63b316bcd4d10cddcae
0vishal <vishalsalaskar72@gmail.com>
2021-05-07 14:44:46 +0530

    [Vishal] Add : program to empty a variable

---
+++ Datastructures/Basic python/Emptyvariable.py
@@ -0,0 +1,17 @@
+'''
+@Author: Vishal Salaskar
+@Date: 2021-05-07 
+@Last Modified by: Vishal Salaskar
+@Last Modified time: 2021-05-07 
+@Title :  To empty a variable without destroying it   
+'''
+def empty_variable():
+    """
+    Description:
+    Function description : To check a variable is defined or not  
+    """   
+    x = 5
+    print(type(x)())
+    
+
+empty_variable()    
