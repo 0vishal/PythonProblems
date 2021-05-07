eddf7ed91cec1f00c744a5f05c2ef9f45821fedb
0vishal <vishalsalaskar72@gmail.com>
2021-05-07 15:00:53 +0530

    [Vishal] Add : program to add zeros to the string

---
+++ Datastructures/Basic python/Fillzeros.py
@@ -0,0 +1,17 @@
+'''
+@Author: Vishal Salaskar
+@Date: 2021-05-07 
+@Last Modified by: Vishal Salaskar
+@Last Modified time: 2021-05-07 
+@Title :  To addd zeros to the string  
+'''
+def fill_zeros():
+    """
+    Description:
+    Function description : To add zeros to the string  
+    """   
+    string = "Welcome"
+    fillzero = string.zfill(10)
+    print(fillzero)
+   
+fill_zeros()    
