97dc296f4b89fae412c56ec664f2cdb798444edb
0vishal <vishalsalaskar72@gmail.com>
2021-05-07 14:40:15 +0530

    [Vishal] Add : program to check if variable is defined or not

---
+++ Datastructures/Basic python/Variabledefine.py
@@ -0,0 +1,20 @@
+'''
+@Author: Vishal Salaskar
+@Date: 2021-05-07 
+@Last Modified by: Vishal Salaskar
+@Last Modified time: 2021-05-07 
+@Title :  To check a variable is defined or not 
+'''
+def variable_define():
+    """
+    Description:
+    Function description : To get numbers divisible by 15
+    """   
+    try:
+        x 
+    except NameError:
+        print("Variable is not defined")
+    else:
+        print("Variable is defined.")
+
+variable_define()    
