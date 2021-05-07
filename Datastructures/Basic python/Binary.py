50029ac8596b7e0e9c950bfa4a1fe10bb73b9d63
0vishal <vishalsalaskar72@gmail.com>
2021-05-07 18:16:53 +0530

    [Vishal] Add : add a program to get key value of dictionary

---
+++ Datastructures/Basic python/Binary.py
@@ -0,0 +1,33 @@
+'''
+@Author: Vishal Salaskar
+@Date: 2021-05-07 
+@Last Modified by: Vishal Salaskar
+@Last Modified time: 2021-05-07 
+@Title :  To get key value of dictionary  
+'''
+import os
+
+def get_binary():
+    """
+    Description:
+    Function description : To get key value of dictionary  
+    """
+    '''
+@Author: Vishal Salaskar
+@Date: 2021-05-07 
+@Last Modified by: Vishal Salaskar
+@Last Modified time: 2021-05-07 
+@Title :  To get key value of dictionary  
+'''
+import os
+
+def get_binary():
+    """
+    Description:
+    Function description : To get key value of dictionary  
+    """
+    num = 5
+    binary = format(num, "010b")
+    print(binary)
+
+get_binary()    
