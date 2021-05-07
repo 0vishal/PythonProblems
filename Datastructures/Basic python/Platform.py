9492fe9be5162dbb09ca67ce0fcfaf7650934985
0vishal <vishalsalaskar72@gmail.com>
2021-05-07 19:28:30 +0530

    [Vishal] Add : program to get the platform architechture

--- Datastructures/Basic python/Platform.py
+++ Datastructures/Basic python/Platform.py
@@ -3,14 +3,14 @@
 @Date: 2021-05-07 
 @Last Modified by: Vishal Salaskar
 @Last Modified time: 2021-05-07 
-@Title :  To get key value of dictionary  
+@Title :  To get the platform architecture   
 '''
 import platform
 
 def get_architechture():
     """
     Description:
-    Function description : To get key value of dictionary  
+    Function description : To get the platform architecture 
     """
     print(platform.architecture())
 
