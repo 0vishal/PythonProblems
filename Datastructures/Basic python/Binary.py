0ad66bd7959e282f9dc30176c1224dea438d7104
0vishal <vishalsalaskar72@gmail.com>
2021-05-07 18:19:54 +0530

    [Vishal] Refactor : modification in program header

--- Datastructures/Basic python/Binary.py
+++ Datastructures/Basic python/Binary.py
@@ -3,28 +3,14 @@
 @Date: 2021-05-07 
 @Last Modified by: Vishal Salaskar
 @Last Modified time: 2021-05-07 
-@Title :  To get key value of dictionary  
+@Title :  To get binary with zeros 
 '''
 import os
 
 def get_binary():
     """
     Description:
-    Function description : To get key value of dictionary  
-    """
-    '''
-@Author: Vishal Salaskar
-@Date: 2021-05-07 
-@Last Modified by: Vishal Salaskar
-@Last Modified time: 2021-05-07 
-@Title :  To get key value of dictionary  
-'''
-import os
-
-def get_binary():
-    """
-    Description:
-    Function description : To get key value of dictionary  
+    Function description : To get binary with zeros  
     """
     num = 5
     binary = format(num, "010b")
