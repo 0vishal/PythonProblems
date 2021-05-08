92b6a9b12c015133a1b8014bc91f597f26dff455
0vishal <vishalsalaskar72@gmail.com>
2021-05-08 12:18:33 +0530

    [Vishal] Add : program to print values of array

---
+++ Datastructures/Array/array.py
@@ -0,0 +1,18 @@
+'''
+@Author: Vishal Salaskar
+@Date: 2021-05-08
+@Last Modified by: Vishal Salaskar
+@Last Modified time: 2021-05-08
+@Title :  To  print the values of array
+'''
+def print_array():
+    """
+    Description:
+    Function description : To  print the values of array
+    """   
+    data = [1,2,3,4,5]
+    for i in data:
+        print(i)
+    
+
+print_array()    
