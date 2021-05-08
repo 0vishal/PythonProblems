003b3f493a846dd507120fa6385e173a13765b5c
0vishal <vishalsalaskar72@gmail.com>
2021-05-08 15:23:17 +0530

    [Vishal] Add : add program to iterate over dictionaries using for loops

---
+++ Datastructures/Dictionary/Iterate.py
@@ -0,0 +1,19 @@
+'''
+@Author: Vishal Salaskar
+@Date: 2021-05-08
+@Last Modified by: Vishal Salaskar
+@Last Modified time: 2021-05-08
+@Title : To iterate a dictionaries using for loops
+'''
+
+def iterate():
+    '''
+    Description : To iterate a dictionaries using for loops
+    '''
+    dict1 = { 1: 2, 2: 4}
+    dict2 = { 3: 6, 4: 3}
+    dict3 = { 5: 9, 6: 1}
+    for i in (dict1,dict2,dict3):
+        print(i)
+
+iterate()    
