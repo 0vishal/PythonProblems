febbe5869c0ca0ab5d17b470da0422f22ba7634a
0vishal <vishalsalaskar72@gmail.com>
2021-05-08 12:29:28 +0530

    [Vishal] Add : program to print the number of occurences in array

---
+++ Datastructures/Array/Count.py
@@ -0,0 +1,18 @@
+'''
+@Author: Vishal Salaskar
+@Date: 2021-05-08
+@Last Modified by: Vishal Salaskar
+@Last Modified time: 2021-05-08
+@Title :  To  print the number of occurences in array 
+'''
+def print_array():
+    """
+    Description:
+    Function description : To  print the number of occurences in array 
+    """   
+    data = [1,2,3,4,5,7,2,4,6,7,2,7]
+    number = 7
+    print(data.count(number))
+    
+
+print_array()    
