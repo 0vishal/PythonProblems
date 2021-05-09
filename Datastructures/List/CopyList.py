38b60a403cc56595537db2c5943277e7281d9b48
0vishal <vishalsalaskar72@gmail.com>
2021-05-09 19:51:14 +0530

    [Vishal] Add : to copy the list

---
+++ Datastructures/List/CopyList.py
@@ -0,0 +1,18 @@
+'''
+@Author: Vishal Salaskar
+@Date: 2021-05-09
+@Last Modified by: Vishal Salaskar
+@Last Modified time: 2021-05-09 
+@Title : To program to copy the list
+'''
+
+def copy_list():
+    """
+    Description:
+    Function description : To program to copy the list
+    """
+    list = [1,2,3,4,5,5,2,3,6]
+    newlist = list.copy()
+    print(newlist)
+   
+copy_list()
