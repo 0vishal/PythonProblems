d8368def2cdea2730f4d149057786bcfbb5160c1
0vishal <vishalsalaskar72@gmail.com>
2021-05-09 19:28:37 +0530

    [Vishal] Add : program to find min max of set

---
+++ Datastructures/Set/Minmax.py
@@ -0,0 +1,19 @@
+'''
+@Author: Vishal Salaskar
+@Date: 2021-05-09 
+@Last Modified by: Vishal Salaskar
+@Last Modified time: 2021-05-09 
+@Title :  program to find min and max of set
+'''
+
+def minmax():
+    """
+    Description:
+    Function description : program find min and max of set.   
+    """
+    data = {}
+    data = {1,2,3,4,5}
+    print(max(data))
+    print(min(data))
+  
+minmax()
