08cffd78efdaa566b3c3bdb42bf29764475ea192
0vishal <vishalsalaskar72@gmail.com>
2021-05-09 23:30:36 +0530

    [Vishal] Add : program to clone the tuple

---
+++ Datastructures/Tuple/Clone.py
@@ -0,0 +1,18 @@
+'''
+@Author: Vishal Salaskar
+@Date: 2021-05-09 
+@Last Modified by: Vishal Salaskar
+@Last Modified time: 2021-05-09 
+@Title :  program to clone the tuple 
+'''
+
+def clone_tuple():
+    """
+    Description:
+    Function description : program to clone the tuple 
+    """
+    data = ("apple", "banana", "cherry")
+    data2 = data
+    print(data2)
+   
+clone_tuple()
