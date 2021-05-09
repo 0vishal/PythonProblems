52cca3fa9fbaaff80c9a2fba02972b877ceee80b
0vishal <vishalsalaskar72@gmail.com>
2021-05-09 18:57:32 +0530

    [Vishal] Add : program to remove over set

---
+++ Datastructures/Set/Remove.py
@@ -0,0 +1,20 @@
+'''
+@Author: Vishal Salaskar
+@Date: 2021-05-09 
+@Last Modified by: Vishal Salaskar
+@Last Modified time: 2021-05-09 
+@Title :  program to remove from set
+'''
+
+def add():
+    """
+    Description:
+    Function description : program to remove from set  
+    """
+    data = {}
+    data = {1,2,3,4,5,6}
+    data.remove(6)
+    for i in data:
+        print(i)
+
+add()
