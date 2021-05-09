1687cfae2be2b304ef34daf6a435d01479b54562
0vishal <vishalsalaskar72@gmail.com>
2021-05-09 19:05:15 +0530

    [Vishal] Add : program to get the intersection of sets

---
+++ Datastructures/Set/Intersection.py
@@ -0,0 +1,18 @@
+'''
+@Author: Vishal Salaskar
+@Date: 2021-05-09 
+@Last Modified by: Vishal Salaskar
+@Last Modified time: 2021-05-09 
+@Title :  program to get the intersection of sets
+'''
+
+def intersection():
+    """
+    Description:
+    Function description : program to get the intersection of sets
+    """
+    data1 = {1,2,3,4,5,6}
+    data2 = {4,5,6,7,8,9}
+    print(data1.intersection(data2))
+
+intersection()
