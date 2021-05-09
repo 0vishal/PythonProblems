be16da6f75ad026e7e4336d676340b75d5490b49
0vishal <vishalsalaskar72@gmail.com>
2021-05-09 18:56:11 +0530

    [Vishal] Add : program to add over set

---
+++ Datastructures/Set/Add.py
@@ -0,0 +1,20 @@
+'''
+@Author: Vishal Salaskar
+@Date: 2021-05-09 
+@Last Modified by: Vishal Salaskar
+@Last Modified time: 2021-05-09 
+@Title :  program to add over the set
+'''
+
+def add():
+    """
+    Description:
+    Function description : program to add over the set  
+    """
+    data = {}
+    data = {1,2,3,4,5}
+    data.add(6)
+    for i in data:
+        print(i)
+
+add()
