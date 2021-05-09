69cac66279ea9aa1948a66bbe64559ee6490ee05
0vishal <vishalsalaskar72@gmail.com>
2021-05-09 19:06:18 +0530

    [Vishal] Add : program to get the union of sets

---
+++ Datastructures/Set/Union.py
@@ -0,0 +1,18 @@
+'''
+@Author: Vishal Salaskar
+@Date: 2021-05-09 
+@Last Modified by: Vishal Salaskar
+@Last Modified time: 2021-05-09 
+@Title :  program to get the union of sets
+'''
+
+def union():
+    """
+    Description:
+    Function description : program to get the union of sets
+    """
+    data1 = {1,2,3,4,5,6}
+    data2 = {4,5,6,7,8,9}
+    print(data1.union(data2))
+
+union()
