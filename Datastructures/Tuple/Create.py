80938003dcab65e58830939c04a578a8da519301
0vishal <vishalsalaskar72@gmail.com>
2021-05-09 21:18:21 +0530

    [Vishal] Add : create a tuple and print it

---
+++ Datastructures/Tuple/Create.py
@@ -0,0 +1,17 @@
+'''
+@Author: Vishal Salaskar
+@Date: 2021-05-09 
+@Last Modified by: Vishal Salaskar
+@Last Modified time: 2021-05-09 
+@Title :  program to create a tuple 
+'''
+
+def create():
+    """
+    Description:
+    Function description : program  to create  a tuple  
+    """
+    data = ("apple", "banana", "cherry")
+    print(data)
+
+create()
