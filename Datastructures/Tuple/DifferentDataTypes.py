f9166a5f96c8f93917ffcaa97a5e101e26d639e5
0vishal <vishalsalaskar72@gmail.com>
2021-05-09 21:20:35 +0530

    [Vishal] Add : create a tuple of different data types

---
+++ Datastructures/Tuple/DifferentDataTypes.py
@@ -0,0 +1,18 @@
+'''
+@Author: Vishal Salaskar
+@Date: 2021-05-09 
+@Last Modified by: Vishal Salaskar
+@Last Modified time: 2021-05-09 
+@Title :  program to create a tuple of different data types
+'''
+
+def create():
+    """
+    Description:
+    Function description : program  to create  a tuple of different data types
+    """
+    data = ("apple", "banana", "cherry", 1, 2, 3)
+    for i in data:
+        print(i)
+
+create()
