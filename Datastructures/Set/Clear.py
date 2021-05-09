ba5b7e3ee5f4e2a78c9f4c2eef3767fa39c1f14e
0vishal <vishalsalaskar72@gmail.com>
2021-05-09 19:12:36 +0530

    [Vishal] Add : program to clear the set

---
+++ Datastructures/Set/Clear.py
@@ -0,0 +1,19 @@
+'''
+@Author: Vishal Salaskar
+@Date: 2021-05-09 
+@Last Modified by: Vishal Salaskar
+@Last Modified time: 2021-05-09 
+@Title :  program to clear set
+'''
+
+def clear():
+    """
+    Description:
+    Function description : program to clear set  
+    """
+    data = {}
+    data = {1,2,3,4,5}
+    data.clear()
+    print(data)
+   
+clear()
