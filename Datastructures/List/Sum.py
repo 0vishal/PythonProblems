cd9f6e9ba065cb0bd31fbface43f2f657d803404
0vishal <vishalsalaskar72@gmail.com>
2021-05-09 19:32:28 +0530

    [Vishal] Add : to get sum of all in list

---
+++ Datastructures/List/Sum.py
@@ -0,0 +1,21 @@
+'''
+@Author: Vishal Salaskar
+@Date: 2021-05-09
+@Last Modified by: Vishal Salaskar
+@Last Modified time: 2021-05-09 
+@Title : To program to sum all the items in a list.
+'''
+
+def Sum():
+    """
+    Description:
+    Function description : To program to sum all the items in a list.
+    """
+    list = [1,2,3,4,5,5,6]
+    sum = 0 
+    for i in list:
+        sum += i
+    print(sum)    
+
+
+Sum()
