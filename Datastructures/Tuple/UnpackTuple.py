449267af6fbd21f1e4b56bf5bbc066d5a13e4aea
0vishal <vishalsalaskar72@gmail.com>
2021-05-09 23:27:30 +0530

    [Vishal] Add : program to unpack tuple into serveral variables

---
+++ Datastructures/Tuple/UnpackTuple.py
@@ -0,0 +1,19 @@
+'''
+@Author: Vishal Salaskar
+@Date: 2021-05-09 
+@Last Modified by: Vishal Salaskar
+@Last Modified time: 2021-05-09 
+@Title :  program to unpack tuple in several variables
+'''
+
+def unpack_tuple():
+    """
+    Description:
+    Function description : program to unpack tuple in several variables
+    """
+    x, y, z = ("apple", "banana", "cherry")
+    print(x)
+    print(y)
+    print(z)
+   
+unpack_tuple()
