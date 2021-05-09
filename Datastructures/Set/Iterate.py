04399e9e74a5b855833481dff908ab5a51bd115f
0vishal <vishalsalaskar72@gmail.com>
2021-05-09 18:53:38 +0530

    [Vishal] Add : program to iterate over set

---
+++ Datastructures/Set/Iterate.py
@@ -0,0 +1,19 @@
+'''
+@Author: Vishal Salaskar
+@Date: 2021-05-09 
+@Last Modified by: Vishal Salaskar
+@Last Modified time: 2021-05-09 
+@Title :  program to iterate over sets. 
+'''
+
+def iterate():
+    """
+    Description:
+    Function description : program to iterate over sets.   
+    """
+    data = {}
+    data = {1,2,3,4,5}
+    for i in data:
+        print(i)
+
+iterate()
