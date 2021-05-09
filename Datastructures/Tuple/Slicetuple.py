1e497f64a8dc4b205f1ca8ea7990ec1d43d964d9
0vishal <vishalsalaskar72@gmail.com>
2021-05-09 23:58:52 +0530

    [Vishal] Add : program to slice a tuple

---
+++ Datastructures/Tuple/Slicetuple.py
@@ -0,0 +1,18 @@
+'''
+@Author: Vishal Salaskar
+@Date: 2021-05-09 
+@Last Modified by: Vishal Salaskar
+@Last Modified time: 2021-05-09 
+@Title : program to slice a tuple
+'''
+
+def slice_tuple():
+    """
+    Description:
+    Function description :  program to slice a tuple
+    """
+    data = (1,2,3,4,5)
+    data2 = data[1:3]
+    print(data2)        
+   
+slice_tuple()
