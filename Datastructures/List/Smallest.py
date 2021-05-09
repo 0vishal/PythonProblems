c6e7106d60f3e5369a633bd4a058c1c4ff3f1fab
0vishal <vishalsalaskar72@gmail.com>
2021-05-09 19:41:32 +0530

    [Vishal] Add : to get smallest value from list

---
+++ Datastructures/List/Smallest.py
@@ -0,0 +1,17 @@
+'''
+@Author: Vishal Salaskar
+@Date: 2021-05-09
+@Last Modified by: Vishal Salaskar
+@Last Modified time: 2021-05-09 
+@Title : To program to get smallest number from list
+'''
+
+def minimum():
+    """
+    Description:
+    Function description : To program to get smallest number from list
+    """
+    list = [1,2,3,4,5,5,6]
+    print(min(list))
+   
+minimum()
