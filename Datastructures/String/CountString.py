27fbc1fa7db95f5b38dc1cf18c2762ec51d073e5
0vishal <vishalsalaskar72@gmail.com>
2021-05-10 17:21:11 +0530

    [Vishal] Add : Program to count the substring in a string

---
+++ Datastructures/String/CountString.py
@@ -0,0 +1,18 @@
+'''
+@Author: Vishal Salaskar
+@Date: 2021-05-10       
+@Last Modified by: Vishal Salaskar
+@Last Modified time: 2021-05-10
+@Title : program to count the substring in a string
+'''
+import textwrap
+
+def count():
+    """
+    Description:
+    Function description : program to count the substring in a string
+    """
+    str1 = "Enter the items with comma seperated: the Vishal salaskar"
+    print(str1.count("the"))
+  
+count()
