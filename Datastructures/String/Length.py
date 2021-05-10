df084889c11a5ce1673cba037c79ff9e56f00334
0vishal <vishalsalaskar72@gmail.com>
2021-05-10 15:59:55 +0530

    [Vishal] Add : Program to calculate the length of string

---
+++ Datastructures/String/Length.py
@@ -0,0 +1,18 @@
+'''
+@Author: Vishal Salaskar
+@Date: 2021-05-10       
+@Last Modified by: Vishal Salaskar
+@Last Modified time: 2021-05-10
+@Title :  program to calculate the length of string 
+'''
+
+def get_length():
+    """
+    Description:
+    Function description : program to calculate the length of string   
+    """
+    data = "Welcome"
+    print(len(data))
+    
+
+get_length()
