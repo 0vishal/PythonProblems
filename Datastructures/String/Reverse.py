313e70480db95f1e7d7c81f0326269aa2ae9f3f9
0vishal <vishalsalaskar72@gmail.com>
2021-05-10 17:24:50 +0530

    [Vishal] Add : Program to reverse the string

---
+++ Datastructures/String/Reverse.py
@@ -0,0 +1,18 @@
+'''
+@Author: Vishal Salaskar
+@Date: 2021-05-10       
+@Last Modified by: Vishal Salaskar
+@Last Modified time: 2021-05-10
+@Title : program to print the string in reverse order
+'''
+import textwrap
+
+def reverse():
+    """
+    Description:
+    Function description : program to print the string in reverse order
+    """
+    str1 = "Enter the items with comma seperated: the Vishal salaskar"[::-1]
+    print(str1)
+  
+reverse()
