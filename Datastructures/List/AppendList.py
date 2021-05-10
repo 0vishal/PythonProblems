c1c0ae15329908e4df95e1e7752bb4c4295f1017
0vishal <vishalsalaskar72@gmail.com>
2021-05-10 09:53:52 +0530

    [Vishal] Add : add program to append list to second list

---
+++ Datastructures/List/AppendList.py
@@ -0,0 +1,22 @@
+'''
+@Author: Vishal Salaskar
+@Date: 2021-05-09
+@Last Modified by: Vishal Salaskar
+@Last Modified time: 2021-05-09 
+@Title : To program to append a list to second list
+'''
+import itertools
+
+def append():
+    """
+    Description:
+    Function description : To program to append a list to second list
+    """
+    data1 = [1,2,3,4,5]
+    data2 = [4,5,6,7]
+    data1.extend(data2)
+    print(data1)
+  
+
+append()   
+
