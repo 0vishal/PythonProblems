4d139ff45be752ab3a4883c64e080d0840beaaf1
0vishal <vishalsalaskar72@gmail.com>
2021-05-10 17:30:44 +0530

    [Vishal] Add : Program to the n lower case character of string

---
+++ Datastructures/String/Lowercase.py
@@ -0,0 +1,17 @@
+'''
+@Author: Vishal Salaskar
+@Date: 2021-05-10       
+@Last Modified by: Vishal Salaskar
+@Last Modified time: 2021-05-10
+@Title : program to print the n lower case characters of string
+'''
+
+def get_lowercase():
+    """
+    Description:
+    Function description : program to print the n lower case characters of string
+    """
+    str1 = "INSTAGRAM"
+    print(str1[:5].lower() + str1[5:])
+  
+get_lowercase()
